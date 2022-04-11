0caa97d6f82d9ab11e15b1d609d4f43567c9acdc
mohdehtishamuddin <mohdehtishamuddin@cedcoss.com>
2022-04-11 20:00:32 +0530

    demo

---
+++ demo.js
@@ -0,0 +1,11 @@
+// let colorInput = document.getElementById('color').value;
+// let HexInput = document.getElementById('Hex').value;
+// colorInput.addEventlistener('input',()=>{
+// let color = colorInput.value;
+// HexInput.value=color;
+// document.body.style.backgroundColor=color;
+// });
+// function changecolor(){
+//     let color = document.getElementById('colorpicker').value;
+//     document.body.style.backgroundColor=color;
+// }
